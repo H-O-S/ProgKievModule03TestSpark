7e67816aeaa3357486d8aea9e97d0732842d7130
Oleg Khrulov <hos@ukr.net>
2019-08-05 19:17:06 +0300

    Project Setup

--- src/main/java/com/khrulov/App.java
+++ src/main/java/com/khrulov/App.java
@@ -1,13 +1,16 @@
 package com.khrulov;
-
 /**
- * Hello world!
+ * Java. My first program on Spark
  *
+ * @author Oleg Khrulov
+ * @version dated 05.08.2019
  */
+
+import static spark.Spark.get;
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        get("/hello", (req, res) -> "Hello World");
     }
 }
