b39141d6cd8b6da05730bc9492c8d3bfc6673b22
Oleg Khrulov <hos@ukr.net>
2019-08-05 18:12:37 +0300

    Initial commit

---
+++ src/main/java/com/khrulov/App.java
@@ -0,0 +1,13 @@
+package com.khrulov;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/khrulov/AppTest.java
@@ -0,0 +1,20 @@
+package com.khrulov;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
