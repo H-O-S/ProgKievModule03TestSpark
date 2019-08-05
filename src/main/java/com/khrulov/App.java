2d5383b528edc2d81fe87e9b4d2f74ff46930903
Oleg Khrulov <hos@ukr.net>
2019-08-05 22:17:59 +0300

    Created a web application that, upon request, returns the current time on the server

--- src/main/java/com/khrulov/App.java
+++ src/main/java/com/khrulov/App.java
@@ -6,11 +6,27 @@ package com.khrulov;
  * @version dated 05.08.2019
  */
 
+/*
+    Создать веб приложение с использованием Spark framework,
+    которое по запросу http://localhost:4567/time вернёт текущее время на сервере
+*/
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+
 import static spark.Spark.get;
-public class App 
-{
-    public static void main( String[] args )
-    {
+
+public class App {
+    public static void main(String[] args) {
+
+
         get("/hello", (req, res) -> "Hello World");
+        get("/time1", (req, res) -> DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now()));
+//        get("/time1", (req, res) -> LocalDateTime.now());
+
+        get("/time2", (req, res) -> Calendar.getInstance().getTime());
+
+
     }
 }
